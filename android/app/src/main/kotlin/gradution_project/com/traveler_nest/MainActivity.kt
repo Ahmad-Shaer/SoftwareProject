02ad9fa7960958788fc91ae327f50171177fe513
Ahmad-Shaer <ahmad4552010@gmail.com>
2023-12-26 19:53:15 +0200

    Remove all files in the branch

--- android/app/src/main/kotlin/gradution_project/com/traveler_nest/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package gradution_project.com.traveler_nest
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- lib/main.dart
+++
@@ -1,5 +0,0 @@
-import 'package:flutter/material.dart';
-
-void main() => runApp(MaterialApp());
-
-int x = 1;
